61b06165e1dfa65e1a1bd88d42ff1d954b5b84b4
Pradyuman kaushal <pradum13@gmail.com>
2019-03-10 16:30:22 +0530

    part a completed

--- blog-post/js/index.js
+++ blog-post/js/index.js
@@ -1,9 +1,22 @@
 var up_modal = document.getElementById('Sign_Up');
 var btn = document.getElementById("SUp");
-var span = document.getElementsByClassName("close")[0];
+btn = document.getElementById("SU");
 
+var span = document.getElementsByClassName("close")[1];
+var in_modal = document.getElementById('Sign_In');
+var ibtn = document.getElementById("SIn");
+var qspan = document.getElementsByClassName("close")[0];
+
+
+ibtn.onclick = function() {
+ in_modal.style.display = "block";
+}
+qspan.onclick = function() {
+  in_modal.style.display = "none";
+}
 btn.onclick = function() {
- up_modal.style.display = "block";
+    in_modal.style.display= "none";
+    up_modal.style.display = "block";
 }
 span.onclick = function() {
   up_modal.style.display = "none";
